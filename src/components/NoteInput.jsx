803f78a77fdcf4518b07be16b25ca38746fe1e92
thedaamanda <thedaamandaa@gmail.com>
2024-01-02 13:45:11 +0700

    fix: note title remaining text more than 50 characters

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import useInput from '../hooks/useInput'
+import useLanguage from '../hooks/useLanguage'
 
 const CHAR_LIMIT = 50;
 
 function NoteInput({ addNote }) {
-    const [title, setTitle] = useInput('');
+    const [title, setTitle] = React.useState('');
     const [body, setBody] = React.useState('');
+
+    const setTitleHandler = (event) => {
+        const title = event.target.value;
+        if (title.length <= CHAR_LIMIT) {
+            setTitle(title);
+        }
+    }
 
     const onBodyChangeHandler = (event) => {
         setBody(event.target.innerHTML);
@@ -20,24 +27,25 @@ function NoteInput({ addNote }) {
         });
     }
 
+    const text = useLanguage('app');
     const remainingTitleLength = CHAR_LIMIT - title.length;
 
     return (
         <form id="note-form" onSubmit={onFormSubmitHandler}>
             <div className="form-groups">
                 <div className="heading">
-                    <span className="tagline">Formulir Catatan</span>
-                    <h2 className="title">Simpan Catatanmu</h2>
+                    <span className="tagline">{text.addNote.tagline}</span>
+                    <h2 className="title">{text.addNote.title}</h2>
                 </div>
                 <div className="field-group">
-                    <div className="title-length">Sisa Karakter: {remainingTitleLength}</div>
-                    <input type="text" placeholder="Isi Judul" id="inputNoteTitle" value={title} onChange={setTitle} required />
+                    <div className="title-length">{text.addNote.remainingCharacters} {remainingTitleLength}</div>
+                    <input type="text" placeholder={text.input.placeholder.title} id="inputNoteTitle" value={title} onChange={setTitleHandler} required />
                 </div>
                 <div className="field-group body-wrapper">
                     <div
                         className="input-body"
                         contentEditable="true"
-                        data-placeholder="Tuliskan catatanmu disini ...."
+                        data-placeholder={text.input.placeholder.body}
                         onInput={onBodyChangeHandler}
                         spellCheck="false"
                         suppressContentEditableWarning={true}
@@ -45,7 +53,7 @@ function NoteInput({ addNote }) {
                     </div>
                 </div>
                 <div className="field-group">
-                    <button value="submit" id="btnSubmitForm">Buat Catatan</button>
+                    <button value="submit" id="btnSubmitForm">{text.addNote.button}</button>
                 </div>
             </div>
         </form>
